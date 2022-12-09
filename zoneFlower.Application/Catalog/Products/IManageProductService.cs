0be279d5674e91463dcc4ff94269d092439142d0
Coder2042002 <phuonghoai090909@gmail.com>
2022-12-09 22:39:29 +0700

    Create application layer structure

---
+++ zoneFlower.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zoneFlower.Application.Catalog.Products.Dtos;
+using zoneFlower.Application.Dtos;
+
+namespace zoneFlower.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        
+        public Task<int> Create(ProductCreateRequest request);
+        public Task<int> Update(ProductEditRequest request);
+        public Task<int> Delete(int productID);
+        public Task<List<ProductViewModel>> GetAll();
+        public Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword,int pageIndex,int pageSize);
+    }
+}
---
+++ zoneFlower.Application/Catalog/Products/ManageProductService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using zoneFlower.Application.Catalog.Products.Dtos;
+using zoneFlower.Application.Dtos;
+using zoneFlower.Data.EF;
+using zoneFlower.Data.Entities;
+
+namespace zoneFlower.Application.Catalog.Products
+{
+    public class ManageProductService : IManageProductService
+    {
+        private readonly ZFlowerDbContext _context;
+        public ManageProductService(ZFlowerDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Create(ProductCreateRequest request)
+        {
+            var product = new Product()
+            {
+                Price = request.Price,
+            };
+            _context.Products.Add(product);
+            return await _context.SaveChangesAsync();
+
+        }
+
+        public Task<int> Delete(int productID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductViewModel>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> Update(ProductEditRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    
+}
