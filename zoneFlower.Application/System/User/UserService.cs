a179caa1b698d497e46f1fed5375777b7f096d58
Coder2042002 <phuonghoai090909@gmail.com>
2022-12-23 23:30:46 +0700

    update login

--- zoneFlower.Application/System/User/UserService.cs
+++ zoneFlower.Application/System/User/UserService.cs
@@ -27,11 +27,12 @@ namespace zoneFlower.Application.System.User
             this._roleManager = roleManager;
             this._config = config;
         }
-        public async Task<string> Authenticate(LoginRequest request)
+        public async Task<string> Authencate(LoginRequest request)
         {
             var user=await _userManager.FindByNameAsync(request.UserName);
-            if (user == null) throw new ZFlowerException("Cannot find user name");
+            if (user == null) 
             return null;
+
             var result =await _signInManager.PasswordSignInAsync(user, request.Password,request.RemeberMe,true);
             if(!result.Succeeded)
             {
@@ -43,7 +44,7 @@ namespace zoneFlower.Application.System.User
             {
                 new Claim(ClaimTypes.Email,user.Email),
                 new Claim(ClaimTypes.GivenName,user.FirstName),
-                new Claim(ClaimTypes.Role,String.Join(";",roles))
+                new Claim(ClaimTypes.Role,string.Join(";",roles))
             };
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Tokens:Key"]));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
